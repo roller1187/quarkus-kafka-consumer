efd1a6c0892cae48ca73c5cfee4804881dda7c04
Andres Romero <aromero@redhat.com>
2019-08-14 02:24:11 -0400

    Improving HTML output

--- src/main/java/org/acme/quarkus/sample/AcrosticConverter.java
+++ src/main/java/org/acme/quarkus/sample/AcrosticConverter.java
@@ -1,10 +1,17 @@
 package org.acme.quarkus.sample;
 
-import io.smallrye.reactive.messaging.annotations.Broadcast;
+import java.util.Base64;
+
+import javax.enterprise.context.ApplicationScoped;
+
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
-import javax.enterprise.context.ApplicationScoped;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
+
+import io.smallrye.reactive.messaging.annotations.Broadcast;
 
 /**
  * A bean consuming data from the "ui-topic" Kafka topic and applying some conversion.
@@ -17,7 +24,17 @@ public class AcrosticConverter {
     @Outgoing("data-stream")
     @Broadcast
     public String process(String acrosticMessage) {
-    	return acrosticMessage;
+		byte[] decodedAcrosticBytes = Base64.getDecoder().decode(acrosticMessage);
+		String decodedAcrosticJSON = new String(decodedAcrosticBytes);
+		
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String prettyJSONString = gson.toJson(new JsonParser().parse(decodedAcrosticJSON));
+		String htmlJSONString;
+		
+		htmlJSONString = prettyJSONString.replaceAll(" ", "&nbsp;");
+		htmlJSONString = htmlJSONString.replaceAll("(\r\n|\n)", "<br />");
+		
+		return htmlJSONString;
     }
 
 }
